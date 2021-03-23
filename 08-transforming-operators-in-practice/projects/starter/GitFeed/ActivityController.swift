c84758280c206656bd1b9d575be94a49923a1de7
Javier Castaneda <jdandini.jc@gmail.com>
2021-03-23 13:08:26 -0600

    Transforming operator in practice completed

--- 08-transforming-operators-in-practice/projects/starter/GitFeed/ActivityController.swift
+++ 08-transforming-operators-in-practice/projects/starter/GitFeed/ActivityController.swift
@@ -39,7 +39,11 @@ class ActivityController: UITableViewController {
   private let repo = "ReactiveX/RxSwift"
 
   private let events = BehaviorRelay<[Event]>(value: [])
+  private let lastModified = BehaviorRelay<String?>(value: nil)
   private let bag = DisposeBag()
+
+  private let eventsFileURL = cachedFileURL("events.json")
+  private let modifiedFileURL = cachedFileURL("modified.txt")
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -53,6 +57,12 @@ class ActivityController: UITableViewController {
     refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
     refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
 
+    let decoder = JSONDecoder()
+    if let eventsData = try? Data(contentsOf: eventsFileURL),
+       let persistedEvents = try? decoder.decode([Event].self, from: eventsData) {
+      events.accept(persistedEvents)
+    }
+
     refresh()
   }
 
@@ -64,11 +74,69 @@ class ActivityController: UITableViewController {
   }
 
   func fetchEvents(repo: String) {
+    let response = Observable.from([repo])
+      .map { urlString -> URL in
+        return URL(string: "https://api.github.com/repos/\(urlString)/events")!
+      }
+      .map { [weak self] url -> URLRequest in
+        var request = URLRequest(url: url)
+        if let modifiedHeader = self?.lastModified.value {
+          request.addValue(modifiedHeader,
+            forHTTPHeaderField: "Last-Modified")
+        }
+        return request
+      }
+      .flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
+        return URLSession.shared.rx.response(request: request)
+      }
+      .share(replay: 1)
+
+    response
+      .filter { response, _ in
+        return 200..<300 ~= response.statusCode
+      }
+      .compactMap { _, data -> [Event]? in
+        return try? JSONDecoder().decode([Event].self, from: data)
+      }
+      .subscribe(onNext: { [weak self] newEvents in
+        self?.processEvents(newEvents)
+      })
+      .disposed(by: bag)
+
+    response
+      .filter { response, _ in
+        return 200..<400 ~= response.statusCode
+      }
+      .flatMap { response, _ -> Observable<String> in
+        guard let value = response.allHeaderFields["Last-Modified"] as? String else {
+          return Observable.empty()
+        }
+        return Observable.just(value)
+      }
+      .subscribe(onNext: { [weak self] modifiedHeader in
+        guard let self = self else { return }
+
+        self.lastModified.accept(modifiedHeader)
+        try? modifiedHeader.write(to: self.modifiedFileURL, atomically: true, encoding: .utf8)
+      })
+      .disposed(by: bag)
 
   }
   
   func processEvents(_ newEvents: [Event]) {
-    
+    var updatedEvents = newEvents + events.value
+    if updatedEvents.count > 50 {
+      updatedEvents = [Event](updatedEvents.prefix(upTo: 50))
+    }
+    events.accept(updatedEvents)
+    DispatchQueue.main.async {
+      self.tableView.reloadData()
+      self.refreshControl?.endRefreshing()
+    }
+    let encoder = JSONEncoder()
+    if let eventsData = try? encoder.encode(updatedEvents) {
+      try? eventsData.write(to: eventsFileURL, options: .atomicWrite)
+    }
   }
 
   // MARK: - Table Data Source
@@ -85,4 +153,11 @@ class ActivityController: UITableViewController {
     cell.imageView?.kf.setImage(with: event.actor.avatar, placeholder: UIImage(named: "blank-avatar"))
     return cell
   }
+}
+
+func cachedFileURL(_ fileName: String) -> URL {
+  return FileManager.default
+    .urls(for: .cachesDirectory, in: .allDomainsMask)
+    .first!
+    .appendingPathComponent(fileName)
 }
